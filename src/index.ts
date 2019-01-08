309ef454045b7f761912c8161b38c02ad229a975
Adam.liu <liuxin.private@gmail.com>
2019-01-08 22:36:55 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import "reflect-metadata";
 import { GraphQLServer } from 'graphql-yoga'
+import { createConnection } from 'typeorm';
 // ... or using `require()`
 // const { GraphQLServer } = require('graphql-yoga')
 
@@ -15,4 +17,7 @@ const resolvers = {
 }
 
 const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on localhost:4000'))
+
+createConnection().then(() => {
+  server.start(() => console.log('Server is running on localhost:4000'))
+})
